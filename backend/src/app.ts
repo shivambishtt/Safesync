fba7db6724a59028d871c9b3f0b28f14bd19cdc7
Shivam <rajshivam2745@gmail.com>
2026-09-20 18:04:20 +0530

    fix(backend): COORS error fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,9 +16,11 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
+    credentials: true,
   }),
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
